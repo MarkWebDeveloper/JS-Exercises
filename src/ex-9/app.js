afd570fa1636cdf3c9c674b6bbe1fc41d69d461b
Mark-Kil <markkilcanarias@gmail.com>
2023-10-28 22:34:59 +0200

    haha, read the task wrong, renamed into ex9

---
+++ src/ex-9/app.js
@@ -0,0 +1,47 @@
+// Escribe un programa que pida una frase y escriba las vocales que aparecen
+
+//Acceder al formulario
+//Hacer una funcion de conteo de las letras
+//Quitar el formulario y poner la cantidad de las vocales
+
+function app() {
+    getForm()
+    submitListener()
+}
+
+function getText() {
+    let textInput = document.getElementById('text').value
+    return textInput
+}
+
+function getForm() {
+    let form = document.getElementById('form')
+    return form
+}
+
+function getVowels() {
+    let vowCount = getText().match(/[aeiou]/gi)
+    return vowCount === null ? 0 : vowCount.length
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    getText()
+    getVowels()
+    form.innerHTML = /* html */ `
+    <h1>El número de las vocales en su frase es ${getVowels()}</h1>
+    <h2>Pues, eso... Ahora lo sabes.</h2>
+    `
+}
+
+function submitListener() {
+    form.addEventListener('submit', onSubmit)
+}
+
+app()
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+
+    
+// })
