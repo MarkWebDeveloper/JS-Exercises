70560e4fefcb94184812f081100f27dd756401e5
Mark-Kil <markkilcanarias@gmail.com>
2023-11-04 18:38:08 +0100

    ex1 description added, first branch commit

--- src/ex-1/app.js
+++ src/ex-1/app.js
@@ -1,3 +1,8 @@
+// Escribe un programa que escriba en la pantalla un texto de tipo <h1> que diga “Hello Javascript”.
+
+// Acceder al elemento title
+// Añadir codigo html
+
 const title = "Hello Javascript"
 const titleSection = document.getElementById('title')
 
