99578aa6337ae798fe544527e2d80d60307fc36a
Mark-Kil <markkilcanarias@gmail.com>
2023-11-04 13:09:47 +0100

    ex12 created

---
+++ src/ex-12/app.js
@@ -0,0 +1,50 @@
+// Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro
+
+//Acceder al formulario.
+//Crear varios condicionales if con % por cada de los numeros.
+//Quitar el formulario y poner la primera condicion que se cumpla.
+
+function app() {
+    getForm()
+    submitListener()
+}
+
+function getNumber() {
+    let numberInput = document.getElementById('number').value
+    return numberInput
+}
+
+function getForm() {
+    let form = document.getElementById('form')
+    return form
+}
+
+function checkIfDivisible() {
+    if (getNumber() % 2 == 0) {
+        return 'Su número es divisible por 2'
+    } else if (getNumber() % 3 == 0) {
+        return 'Su número es divisible por 3'
+    } else if (getNumber() % 5 == 0) {
+        return 'Su número es divisible por 5'
+    } else if (getNumber() % 7 == 0) {
+        return 'Su número es divisible por 7'
+    } else {
+        return 'Su número no es divisible por 2, 3, 5 o 7'
+    }
+}
+
+function onSubmit(event) {
+    event.preventDefault();
+    getNumber()
+    checkIfDivisible()
+    form.innerHTML = /* html */ `
+    <h1>${checkIfDivisible()}</h1>
+    <h2>Por cierto, ¿no lo podías calcular tú mismo?</h2>
+    `
+}
+
+function submitListener() {
+    form.addEventListener('submit', onSubmit)
+}
+
+app()
