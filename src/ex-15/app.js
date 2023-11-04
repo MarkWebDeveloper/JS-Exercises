8b335ad033cc2d8760e01c2d7f4f514b7200c315
Mark-Kil <markkilcanarias@gmail.com>
2023-11-04 18:35:33 +0100

    ex15 ready

--- src/ex-15/app.js
+++ src/ex-15/app.js
@@ -1,25 +1,23 @@
-// Escribir un programa que escriba en pantalla los divisores comunes de dos números dados
+// Escribir un programa que nos diga si un número dado es primo (no es divisible por ninguno otro número que no sea él mismo o la unidad)
 
-// Como en el ejercicio anterior, calcular los divisores de cada numero
-// Añadir todos los divisores a una lista
-// Filtrar la lista para dejar solo los divisores comunes
-// Crear una funcion para eliminar los duplicados
-// Imprimirlo todo en la pantalla
+// Aquí tiene que ser más fácil. También hacemos una lista con los divisores.
+// Ahora si en la lista hay solo dos elementos, el mismo numero y 1, entonces el numero es primo. En el caso contrario, no lo es.
 
 function app() {
     getForm()
     submitListener()
 }
 
-let number1 = document.getElementById('number1').value
-let number2 = document.getElementById('number2').value
 let divisorsArray = []
-let stringArray = []
-let numbersString = ''
 
 function getForm() {
     let form = document.getElementById('form')
     return form
+}
+
+function getNumber() {
+    let numberInput = document.getElementById('number').value
+    return numberInput
 }
 
 function findDivisors(num) {
@@ -29,52 +27,30 @@ function findDivisors(num) {
     }
 }
 
-function getNotUnique() {
-    map = new Map()
-    divisorsArray.forEach(a => map.set(a, (map.get(a) || 0) + 1))
-    return divisorsArray.filter(a => map.get(a) > 1);
-}
-
-function deleteDuplicates() {
-    return [...new Set(getNotUnique())]
-}
-
-function composeStringFromArray() {
-    for (let index = 0; index < deleteDuplicates().length; index++) {
-        const element = deleteDuplicates()[index];
-        stringArray.push(element, ", ")
+function checkIfPrime() {
+    if (divisorsArray.length === 2) {
+        return true
+    } else {
+        return false
     }
-
-    if (stringArray[stringArray.length - 1] === ', ') {
-        stringArray.pop()
-    }
-
-    if (stringArray.length > 2) {
-        const yLetter = " y "
-        stringArray[stringArray.length - 2] = yLetter
-    }
-
-    numbersString = stringArray.join('')
 }
 
 function onSubmit(event) {
     event.preventDefault();
-    let number1 = document.getElementById('number1').value
-    let number2 = document.getElementById('number2').value
-    findDivisors(number1)
-    findDivisors(number2)
-    getNotUnique()
-    deleteDuplicates()
-    composeStringFromArray()
+    getNumber()
+    findDivisors(getNumber())
+    checkIfPrime()
 
-    if (stringArray.length === 0) {
+    if (checkIfPrime() === true) {
         form.innerHTML = /* html */ `
-        <h1>Los números introducidos no tienen divisores comunes</h1>
+        <h1>Sí, el número ${getNumber()} es primo</h1>
+        <h2>¿Estás contento?</h2>
         `
+
     } else {
         form.innerHTML = /* html */ `
-        <h1>Los divisores comunes de ${number1} y ${number2} son ${numbersString}</h1>
-        <h2>Así que soy un programa muy útil</h2>
+        <h1>No, el número ${getNumber()} no es primo</h1>
+        <h2>Y aquí empieza a sonar la música triste</h2>
         `
     }
 }
